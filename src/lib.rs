29dc31a406b1897b5e10c66c704fcfb468685300
Filipp Samoilov <samoylovfp@gmail.com>
2024-06-30 09:45:06 +0300

    Anti-alias before finding max
    Fixes the issue with the algorithm missing the frequencies
    that fall between the fft buckets.

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@
 //!         use std::f32::consts::TAU;
 //!         (i as f32 / 44100.0 * 440.0 * TAU).sin()
 //!         // noise
-//!         + 0.5 * (i as f32 / 44100.0 * 100.0 * TAU).sin()
-//!         + 0.5 * (i as f32 / 44100.0 * 120.0 * TAU).sin()
+//!         + 0.9 * (i as f32 / 44100.0 * 100.0 * TAU).sin()
+//!         + 0.9 * (i as f32 / 44100.0 * 120.0 * TAU).sin()
 //!     })
 //!     .collect::<Vec<_>>();
 //!
@@ -40,17 +40,17 @@ impl FreqDetector {
     /// `sample_count` numbers between `2048` and `8192` work well.
     /// More samples usually means more accuracy, but requires more audio,
     /// which also means more latency for realtime application.
-    /// 
+    ///
     /// # Errors
     /// - if sample rate is 0
     /// - if fewer than 4 samples are passed
     pub fn new(sample_rate: usize, sample_count: usize) -> Result<Self, DetectorCreateError> {
         let mut planner = FftPlanner::new();
         if sample_rate < 1 {
-            return Err(DetectorCreateError::SampleRateTooLow)
+            return Err(DetectorCreateError::SampleRateTooLow);
         }
         if sample_count < 4 {
-            return Err(DetectorCreateError::TooFewSamples)
+            return Err(DetectorCreateError::TooFewSamples);
         }
         Ok(Self {
             fft: planner.plan_fft_forward(sample_count),
@@ -71,43 +71,41 @@ impl FreqDetector {
                 passed: samples.len(),
             });
         }
-        let mut buf = samples
+        let mut fft_buf = samples
             .iter()
             .copied()
             .map(|s| Complex { re: s, im: 0.0 })
             .collect::<Vec<_>>();
 
-        self.fft.process(&mut buf);
+        self.fft.process(&mut fft_buf);
 
-        let peak = buf
+        let antialised_power_values = fft_buf
+            .windows(2)
+            // only interested in positive frequencies
+            .take(self.sample_count / 2)
+            .map(|w| [w[0].abs(), w[1].abs()])
+            .enumerate()
+            .collect::<Vec<_>>();
+
+        let peak_window = antialised_power_values
             .iter()
             .copied()
-            .enumerate()
-            .take(self.sample_count / 2)
-            .max_by_key(|(_, s)| (s.abs() * 1000.0) as u32)
-            .expect("to have at least 1 sample");
-        if peak.1.abs() < 0.00001 {
+            .max_by(|c1, c2| {
+                c1.1.iter()
+                    .sum::<f32>()
+                    .total_cmp(&c2.1.iter().sum::<f32>())
+            })
+            .expect("to have at least 1 positive frequency");
+
+        if peak_window.1.iter().sum::<f32>() < 0.0001 {
             return Ok(0.0);
         }
 
-        // use neighbors for anti-aliasing
-        let mut neighbors = Vec::with_capacity(3);
-        neighbors.push(peak);
-        if peak.0 > 1 {
-            neighbors.push((peak.0 - 1, buf[peak.0 - 1]));
-        }
-        if peak.0 < (self.sample_count / 2 - 1) {
-            neighbors.push((peak.0 + 1, buf[peak.0 + 1]));
-        }
-
-        neighbors.sort_unstable_by(|c1, c2| c1.1.abs().total_cmp(&c2.1.abs()).reverse());
-        // only take the two top values
-        neighbors.truncate(2);
         // take weighted average of the two biggest
         // not sure what is the math behind why this works, but it does
-        let res = (self.fft_bucket_to_freq(neighbors[0].0) * neighbors[0].1.abs()
-            + self.fft_bucket_to_freq(neighbors[1].0) * neighbors[1].1.abs())
-            / (neighbors[0].1.abs() + neighbors[1].1.abs());
+        let res = (self.fft_bucket_to_freq(peak_window.0) * peak_window.1[0]
+            + self.fft_bucket_to_freq(peak_window.0 + 1) * peak_window.1[1])
+            / peak_window.1.iter().sum::<f32>();
         if res.is_nan() {
             Err(DetectError::NansFound)
         } else {
@@ -133,7 +131,7 @@ pub enum DetectorCreateError {
     #[error("Detector does not support sample rate < 1 sample per second")]
     SampleRateTooLow,
     #[error("Needs at least 4 samples for detection")]
-    TooFewSamples
+    TooFewSamples,
 }
 
 #[cfg(test)]
@@ -151,8 +149,8 @@ mod tests {
                 .map(|i| {
                     (i as f32 / 44100.0 * freq as f32 * TAU).sin()
                         // noise
-                        + 0.3 * (i as f32 / 44100.0 * 101.0 * TAU).sin()
-                        + 0.3 * (i as f32 / 44100.0 * 120.0 * TAU).sin()
+                        + 0.9 * (i as f32 / 44100.0 * 101.0 * TAU).sin()
+                        + 0.9 * (i as f32 / 44100.0 * 120.0 * TAU).sin()
                 })
                 .collect::<Vec<_>>();
 
